3bb648ea6e1ff1ea56839e022080d31c5adfa7df
ScopeX-ASU <jiaqigu@asu.edu>
2023-12-15 00:18:18 -0700

    [v0.0.2] step_count bug fix

--- pyutils/optimizer/sam.py
+++ pyutils/optimizer/sam.py
@@ -22,6 +22,7 @@ class SAM(torch.optim.Optimizer):
 
         self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
         self.param_groups = self.base_optimizer.param_groups
+        self._step_count = 0
 
     @torch.no_grad()
     def first_step(self, zero_grad=False):
