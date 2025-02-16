e555a5121922cc172e4fbd06b97abc364ededfac
JeremieMelo <jiaqigu@asu.edu>
2025-02-16 14:27:47 -0700

    new version

--- pyutils/version.py
+++ pyutils/version.py
@@ -3,8 +3,8 @@ Description:
 Author: Jiaqi Gu (jqgu@utexas.edu)
 Date: 2021-11-29 04:15:53
 LastEditors: Jiaqi Gu && jiaqigu@asu.edu
-LastEditTime: 2024-06-16 12:31:47
+LastEditTime: 2024-12-04 21:34:07
 '''
-__version__ = "0.0.3"
+__version__ = "0.0.3.2"
 
 __all__ = ["__version__"]
